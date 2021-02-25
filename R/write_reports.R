3d29dff40b6f107022896f60b2259a855b03a480
keertic <keer.chalasani@gmail.com>
2021-02-24 21:05:35 -0500

    Update write_reports.R

--- R/write_reports.R
+++ R/write_reports.R
@@ -58,13 +58,13 @@ write_reports <- function(username, password, table, mft, start, end, directory=
   # writing data table below
   writeDataTable(wb, sheet1,
                  state_req_nulls %>% 
-                   right_join(fnames, ., by = "Facility"),
+                   right_join(fnames, ., by=c("C_Facility_ID" = "Facility")),
                  startCol=1, startRow=3, bandedRows=TRUE)
   # formatting widths, freeze panes, and color
-  setColWidths(wb, sheet1, 1:ncol(right_join(fnames, state_req_nulls, by = "Facility")), "auto")
+  setColWidths(wb, sheet1, 1:ncol(right_join(fnames, state_req_nulls, by=c("C_Facility_ID" = "Facility")), "auto")
   freezePane(wb, sheet1, firstActiveRow=4, firstActiveCol=4)
   addStyle(wb, sheet1, createStyle(fgFill="#4f81bd", fontColour="#ffffff", textDecoration = "bold"),
-           rows=1:3, cols=1:ncol(right_join(fnames, state_req_nulls, by = "Facility")), gridExpand=TRUE)
+           rows=1:3, cols=1:ncol(right_join(fnames, state_req_nulls, by=c("C_Facility_ID" = "Facility"))), gridExpand=TRUE)
   # sheet 2: optional nulls
   sheet2 <- addWorksheet(wb, "Optional Nulls")
   # putting statewide above the filter
@@ -73,13 +73,13 @@ write_reports <- function(username, password, table, mft, start, end, directory=
   # writing data table below
   writeDataTable(wb, sheet2,
                  state_opt_nulls %>% 
-                   right_join(fnames, ., by = "Facility"),
+                   right_join(fnames, ., by=c("C_Facility_ID" = "Facility")),
                  startCol=1, startRow=3, bandedRows=TRUE)
   # formatting widths, freeze panes, and color
-  setColWidths(wb, sheet2, 1:ncol(right_join(fnames, state_opt_nulls, by = "Facility")), "auto")
+  setColWidths(wb, sheet2, 1:ncol(right_join(fnames, state_opt_nulls, by=c("C_Facility_ID" = "Facility"))), "auto")
   freezePane(wb, sheet2, firstActiveRow=4, firstActiveCol=4)
   addStyle(wb, sheet2, createStyle(fgFill="#4f81bd", fontColour="#ffffff", textDecoration = "bold"),
-           rows=1:3, cols=1:ncol(right_join(fnames, state_opt_nulls, by = "Facility")), gridExpand=TRUE)
+           rows=1:3, cols=1:ncol(right_join(fnames, state_opt_nulls, by=c("C_Facility_ID" = "Facility"))), gridExpand=TRUE)
   # sheet 3: invalids
   sheet3 <- addWorksheet(wb, "Invalids")
   # putting statewide above the filter
